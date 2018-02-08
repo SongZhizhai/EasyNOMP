cd2568a70863adee8ca3a82850390bb8b52ea92e
devnulled <devnulled@cryptaz.org>
2018-02-09 06:12:12 +0700

    fuck you, brain, you don't need sleep

--- libs/paymentProcessor.js
+++ libs/paymentProcessor.js
@@ -477,7 +477,7 @@ function SetupForPool(poolOptions, setupFinished) {
                     logger.info('Payments to miners: %s', JSON.stringify(addressAmounts));
 
                     Object.keys(addressAmounts).forEach((address) => {
-                        addressAmounts[address].amount = addressAmounts[address].amount.toFixed(coinPrecision).toString(10)
+                        addressAmounts[address] = addressAmounts[address].toFixed(coinPrecision).toString(10)
                     });
 
                     logger.info('Ok, going to pay from "%s" address with final amounts: %s', addressAccount, JSON.stringify(addressAmounts));
