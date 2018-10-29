1f3ecf7f66e0d276c228fd9c96f62ee9dcf853d3
LeshaCat <43627985+leshacat@users.noreply.github.com>
2018-10-29 03:47:30 -0400

    fix miner_stats

--- libs/paymentProcessor.js
+++ libs/paymentProcessor.js
@@ -345,7 +345,7 @@ function SetupForPool(poolOptions, setupFinished) {
       function(workers, rounds, callback) {
         logger.debug("Checking for confirmed rounds (blocks)");
         
-		if (rounds !== null) {
+		if (rounds !== null & rounds !== "" & rounds !== "[]") {
         
 		        var batchRPCcommand = rounds.map(function(r) {
 		        	return ['gettransaction', [r.txHash]];
